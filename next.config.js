80de5b18afe49e4a98b42baffbab4f56be2bf73c
Paul Silva <psilva8@gmail.com>
2025-05-22 15:11:56 -0700

    Fix Next.js config by using only valid image formats (webp, avif)

--- next.config.js
+++ next.config.js
@@ -34,7 +34,7 @@ const nextConfig = {
     unoptimized: true,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ['image/webp', 'image/jpeg'],
+    formats: ['image/webp', 'image/avif'],
     minimumCacheTTL: 60,
     remotePatterns: [
       {
